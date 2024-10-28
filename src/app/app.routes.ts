17c87e33c54f12fca00ac98f41976c781e39b8d2
Fedecardozo <Fedecardozo.a@gmail.com>
2024-10-28 16:54:07 -0300

    rutas definidas del login y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,16 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./home/home.component').then((m) => m.HomeComponent),
   },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'registro',
+    loadComponent: () =>
+      import('./pages/registro/registro.component').then(
+        (m) => m.RegistroComponent
+      ),
+  },
 ];
