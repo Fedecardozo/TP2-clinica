db4bf5a7109e76c45f7f36cd6d4966a14c7200d5
Fedecardozo <Fedecardozo.a@gmail.com>
2024-10-28 17:58:39 -0300

    login con validaciones y tambien agregue firebase

---
+++ src/app/models/alert.ts
@@ -0,0 +1,23 @@
+import Swal from 'sweetalert2';
+
+export class Alert {
+  static exito(titulo: string = '', texto: string = '') {
+    return Swal.fire({
+      title: titulo,
+      text: texto,
+      icon: 'success',
+      backdrop: true, // Esta opción asegura que el fondo sea oscuro
+      allowOutsideClick: false, // Impide cerrar al hacer clic fuera del modal
+    });
+  }
+
+  static error(titulo: string = '', texto: string = '') {
+    return Swal.fire({
+      title: titulo,
+      text: texto,
+      icon: 'error',
+      backdrop: true, // Esta opción asegura que el fondo sea oscuro
+      allowOutsideClick: false, // Impide cerrar al hacer clic fuera del modal
+    });
+  }
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Alert } from '../../models/alert';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  public fb: FormBuilder = inject(FormBuilder);
+  public fg: FormGroup;
+  private userService: AuthService = inject(AuthService);
+  private router = inject(Router);
 
+  constructor() {
+    this.fg = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]],
+      clave: ['', [Validators.required]],
+    });
+  }
+
+  accesoRapido(mail: string, password: string) {
+    this.fg.controls['correo'].setValue(mail);
+    this.fg.controls['clave'].setValue(password);
+  }
+
+  acceder() {
+    if (this.fg.valid) {
+      this.userService
+        .login(
+          this.fg.controls['correo'].value,
+          this.fg.controls['clave'].value
+        )
+        .then(() => {
+          this.router.navigateByUrl('/home');
+        })
+        .catch(() => {
+          //Muestro un alert de que no esta registrado
+          Alert.error(
+            'No se encuentra registrado',
+            'Verifique correo y contraseña ingresadas'
+          );
+        })
+        .finally(() => {
+          this.fg.reset();
+        });
+    }
+  }
 }
