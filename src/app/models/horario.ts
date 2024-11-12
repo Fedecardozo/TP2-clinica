f7ccc1020a8a3784beb47c9e61b888901199b866
Fedecardozo <Fedecardozo.a@gmail.com>
2024-11-12 12:14:39 -0300

    filtro los horarios y fechas de los turnos que esta reservado

--- src/app/models/horario.ts
+++ src/app/models/horario.ts
@@ -8,4 +8,15 @@ export class Horario {
     this.horario = horario;
     this.hora_seleccionada = '';
   }
+
+  static convertirFecha(fecha_number: string) {
+    const timestampSoloFecha = parseInt(fecha_number); // Ejemplo de timestamp en milisegundos
+
+    const fecha = new Date(timestampSoloFecha);
+    const fechaLegible = `${fecha.getFullYear()}-${(fecha.getMonth() + 1)
+      .toString()
+      .padStart(2, '0')}-${fecha.getDate().toString().padStart(2, '0')}`;
+
+    return fechaLegible;
+  }
 }
--- src/app/pages/sacar-turno/dia/dia.component.ts
+++ src/app/pages/sacar-turno/dia/dia.component.ts
@@ -3,6 +3,9 @@ import { Usuario } from '../../../models/usuario';
 import { DatePipe, TitleCasePipe } from '@angular/common';
 import { UtilsService } from '../../../services/utils.service';
 import { Horario } from '../../../models/horario';
+import { Subscription } from 'rxjs';
+import { FirebaseService } from '../../../services/firebase.service';
+import { Turno } from '../../../models/turno';
 
 @Component({
   selector: 'app-dia',
@@ -20,6 +23,8 @@ export class DiaComponent {
   @Output() back = new EventEmitter<void>();
   util = inject(UtilsService);
   horarios: string[] = [];
+  sub?: Subscription;
+  fire = inject(FirebaseService);
 
   goBack() {
     this.back.emit();
@@ -28,6 +33,43 @@ export class DiaComponent {
   ngOnInit(): void {
     this.generar_horarios();
     this.generarDias();
+    this.filtrar_dia_horario();
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+
+  filtrar_dia_horario() {
+    this.sub = this.fire
+      .getCollection('turnos')
+      .valueChanges()
+      .subscribe((next) => {
+        const aux = next as Turno[];
+        const turnos_especialiasta = aux.filter(
+          (item) => item.id_especialista === this.especialista?.id
+        );
+
+        turnos_especialiasta.forEach((item) => {
+          for (let index = 0; index < this.dia_horario.length; index++) {
+            const element = this.dia_horario[index];
+            //Obtengo la fecha que se genero ahora
+            const fecha_actual = element.fecha.getTime().toString();
+            //La formateo a un fecha legible
+            const fecha_nueva = Horario.convertirFecha(fecha_actual);
+            //Formateo la fecha reservada del especialista en legible
+            const fecha_reservada = Horario.convertirFecha(item.fecha);
+            //Saco del array los horarios y si es necesario la fecha
+            if (fecha_nueva === fecha_reservada) {
+              const indice_hora = element.horario.indexOf(item.hora);
+              element.horario.splice(indice_hora, indice_hora + 1);
+              //Saco la fecha si no tiene horarios para esa fecha
+              if (!element.horario.length)
+                this.dia_horario.splice(index, index + 1);
+            }
+          }
+        });
+      });
   }
 
   generarDias() {
@@ -64,7 +106,7 @@ export class DiaComponent {
           : 30;
 
       this.horarios = [
-        ...this.util.generar_horarios(inicio - 1, fin, fin_minutos),
+        ...this.util.generar_horarios(inicio - 1, fin - 1, fin_minutos),
       ];
     }
   }
