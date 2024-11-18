06d38069ce471863052990e7d2094e062aac4c30
Fedecardozo <Fedecardozo.a@gmail.com>
2024-11-18 20:10:49 -0300

    filtrar por fecha funcionando

--- src/app/pages/admin/graficos/graficos.component.ts
+++ src/app/pages/admin/graficos/graficos.component.ts
@@ -5,27 +5,49 @@ import { FirebaseService } from '../../../services/firebase.service';
 import { TitleCasePipe } from '@angular/common';
 import { BarraComponent } from './barra/barra.component';
 import { LineComponent } from './line/line.component';
+import { Turno } from '../../../models/turno';
+import { MedicoBarraSolicitadoComponent } from './medico-barra-solicitado/medico-barra-solicitado.component';
+import { MedicoBarraFinalizadoComponent } from './medico-barra-finalizado/medico-barra-finalizado.component';
+import { Chart, ChartType } from 'chart.js/auto';
 
 @Component({
   selector: 'app-graficos',
   standalone: true,
-  imports: [TitleCasePipe, BarraComponent, LineComponent],
+  imports: [
+    TitleCasePipe,
+    BarraComponent,
+    LineComponent,
+    MedicoBarraSolicitadoComponent,
+    MedicoBarraFinalizadoComponent,
+  ],
   animations: [slideUpAnimation],
   templateUrl: './graficos.component.html',
   styleUrl: './graficos.component.css',
 })
 export class GraficosComponent {
   sub?: Subscription;
+  sub2?: Subscription;
   fire = inject(FirebaseService);
   logs: any[] = [];
   th: string[] = [];
+  turnos: Turno[] = [];
+  turnos_solicitados: any[] = [];
+  turnos_finalizados: any[] = [];
+  especialista: string[] = [];
+  cantidad: number[] = [];
 
   constructor() {
     this.th = ['Correo', 'Nombre', 'Apellido', 'Fecha', 'Dia', 'Horario'];
   }
 
-  ngOnInit(): void {
+  async ngOnInit() {
     this.getLogs();
+    await this.getTurnos();
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+    this.sub2?.unsubscribe();
   }
 
   getLogs() {
@@ -34,6 +56,43 @@ export class GraficosComponent {
       .valueChanges()
       .subscribe((next) => {
         this.logs = next;
+      });
+  }
+
+  async getTurnos() {
+    await this.fire
+      .getCollection('turnos')
+      .get()
+      .forEach((next) => {
+        next.docs.forEach((item) => {
+          this.turnos.push(item.data() as Turno);
+        });
+
+        //Solo especialidades
+        const tur_especialistas = this.turnos.filter(
+          (item, index, self) =>
+            index ===
+            self.findIndex((t) => t.id_especialista === item.id_especialista)
+        );
+
+        //Cargo especialistas
+        tur_especialistas.forEach((item) => {
+          this.especialista.push(item.especialista);
+        });
+
+        //Cargar cantidad
+        tur_especialistas.forEach(() => {
+          this.cantidad.push(0);
+        });
+
+        //Cantidad turnos especialistas
+        tur_especialistas.forEach((espe, index) => {
+          this.turnos.forEach((item) => {
+            if (item.id_especialista === espe.id_especialista) {
+              this.cantidad[index]++;
+            }
+          });
+        });
       });
   }
 
---
+++ src/app/pages/admin/graficos/medico-barra-solicitado/medico-barra-solicitado.component.ts
@@ -0,0 +1,151 @@
+import { Component, Input } from '@angular/core';
+import { Chart, ChartType } from 'chart.js/auto';
+import { Turno } from '../../../../models/turno';
+import { Alert } from '../../../../models/alert';
+
+@Component({
+  selector: 'app-medico-barra-solicitado',
+  standalone: true,
+  imports: [],
+  templateUrl: './medico-barra-solicitado.component.html',
+  styleUrl: './medico-barra-solicitado.component.css',
+})
+export class MedicoBarraSolicitadoComponent {
+  chart?: Chart;
+  @Input() turnos: Turno[] = [];
+  especialista: string[] = [];
+  cantidad: number[] = [];
+  filtro_turnos: Turno[] = [];
+
+  ngOnInit() {
+    this.generarDatosChart();
+    this.crearChart();
+  }
+
+  generarDatosChart(fecha1: Date | null = null, fecha2: Date | null = null) {
+    //Solo especialidades
+    const tur_especialistas = this.turnos.filter(
+      (item, index, self) =>
+        index ===
+        self.findIndex((t) => t.id_especialista === item.id_especialista)
+    );
+
+    //Cargo especialistas
+    tur_especialistas.forEach((item) => {
+      this.especialista.push(item.especialista);
+    });
+
+    //Cargar cantidad
+    tur_especialistas.forEach(() => {
+      this.cantidad.push(0);
+    });
+
+    if (fecha1 && fecha2) {
+      //Cantidad turnos especialistas
+      tur_especialistas.forEach((espe, index) => {
+        this.turnos.forEach((item) => {
+          const fecha = new Date(parseInt(item.fecha));
+
+          if (
+            item.id_especialista === espe.id_especialista &&
+            item.estado === Turno.estado_pediente &&
+            fecha > fecha1 &&
+            fecha <= fecha2
+          ) {
+            this.cantidad[index]++;
+          }
+        });
+      });
+    } else {
+      //Cantidad turnos especialistas
+      tur_especialistas.forEach((espe, index) => {
+        this.turnos.forEach((item) => {
+          if (
+            item.id_especialista === espe.id_especialista &&
+            item.estado === Turno.estado_pediente
+          ) {
+            this.cantidad[index]++;
+          }
+        });
+      });
+    }
+  }
+
+  filtrar() {
+    const fecha_inicial = document.getElementById(
+      'fecha_inicial'
+    ) as HTMLInputElement;
+    const fecha_final = document.getElementById(
+      'fecha_final'
+    ) as HTMLInputElement;
+
+    if (fecha_inicial.value && fecha_final.value) {
+      const fecha_1 = new Date(fecha_inicial.value);
+      const fecha_2 = new Date(fecha_final.value);
+      this.especialista.splice(0, this.especialista.length);
+      this.cantidad.splice(0, this.cantidad.length);
+
+      this.generarDatosChart(fecha_1, fecha_2);
+      this.crearChart();
+    } else Alert.msjTimer('No cargo las fechas', 'error');
+  }
+
+  crearChart() {
+    const canvas = document.getElementById('chart4') as HTMLCanvasElement;
+
+    if (!canvas) {
+      console.error('Canvas element not found!');
+      return;
+    }
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) {
+      console.error('Failed to get 2D context!');
+      return;
+    }
+
+    // Destruye la instancia previa si existe
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
+    const data = {
+      labels: this.especialista,
+      datasets: [
+        {
+          label: 'Cantidad',
+          data: this.cantidad,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    // Crear el gráfico
+    this.chart = new Chart(ctx, {
+      type: 'bar' as ChartType, // Tipo de gráfica
+      data, // Datos
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+  }
+}
